9ef0d3886d1c822009c49189fb52ca9f4cdb1bc4
i1nam <145010999+i1nam@users.noreply.github.com>
2024-01-25 00:55:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { Hercai } = require('hercai');
 const Monitor = require('ping-monitor');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 const client = new Hercai();
 
